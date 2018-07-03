12c687ec4baf7a539e53f206c56c6d044a626054
BhanuKishoreSingh <bhanukishore29@yahoo.in>
2018-07-03 07:27:00 +0000

    Committed the login page code

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
 
-import App from "./app";
-
-console.log('hello');
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
